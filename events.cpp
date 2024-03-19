0e3eb7b941f3aa2b23dc604dbe4ea8aa60199ab5
kazarovetss <136485938+kazarovetss@users.noreply.github.com>
2024-03-19 11:15:34 +0300

    Add files via upload

---
+++ events.cpp
@@ -0,0 +1,65 @@
+#include "events.h"
+#include "ui_mainwindow.h"
+Events::Events()
+{
+
+}
+
+CustomGraphicsView::CustomGraphicsView(QWidget *widget):QGraphicsView(widget)
+{
+
+    QGraphicsScene *scene = new QGraphicsScene(this);
+        this->setScene(scene);
+    setAcceptDrops(true);
+
+}
+
+void CustomGraphicsView::dropEvent(QDropEvent *event)
+{
+    if (event->mimeData()->hasFormat("application/vnd.text.listwidgetitem")) {
+
+            QByteArray itemData = event->mimeData()->data("application/vnd.text.listwidgetitem"); // Извлечение данных из события
+            QDataStream dataStream(&itemData, QIODevice::ReadOnly);
+
+            QPixmap pixmap; // Чтение pixmap и текста из потока данных
+            QString text;
+            dataStream >> pixmap >> text;
+
+            QGraphicsPixmapItem *item = new QGraphicsPixmapItem(pixmap); // Создание и добавление нового элемента сцены
+            item->setFlag(QGraphicsItem::ItemIsSelectable, true);
+            item->setData(0, text);
+            item->setPos(0, 0); // Установка начальной позиции элемента на (0,0)
+            scene()->addItem(item);
+            event->acceptProposedAction();
+            emit itemDropped(text, QPointF(0, 0)); // Испускание сигнала с координатами (0,0)
+
+           }
+}
+
+void CustomGraphicsView::dragEnterEvent(QDragEnterEvent *event)
+{
+    if (event->mimeData()->hasFormat("application/vnd.text.listwidgetitem")) {
+        event->acceptProposedAction(); // Принятие действия
+    }
+}
+
+void CustomGraphicsView::dragMoveEvent(QDragMoveEvent *event)
+{
+    if (event->mimeData()->hasFormat("application/vnd.text.listwidgetitem")) { // Проверка формата и принятие действия
+        event->acceptProposedAction();
+    }
+}
+
+void CustomGraphicsView::getResize() { // Слот для обновления размера сцены
+
+    scene()->setSceneRect(0, 0, width()-2, height()-2);// Установка размера сцены равным размеру виджета минус 2 пикселя с каждой стороны
+}
+
+void CustomGraphicsView::resizeEvent(QResizeEvent *event) { //Обработчик события изменения размера виджета
+    QGraphicsView::resizeEvent(event);
+    getResize(); // Обновление размера сцены
+}
+
+
+
+
---
+++ mainwindow.cpp
@@ -0,0 +1,75 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMimeData>
+#include <QDrag>
+#include <QStringListModel>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+
+    ui->setupUi(this);
+    model = new QStringListModel(this); //Установка модели для QListView
+    ui->listView->setModel(model);
+    customGraphicsView = qobject_cast<CustomGraphicsView*>(ui->graphicsView); //Инициализация QGraphicsView созданного в Design
+
+    connect(customGraphicsView, &CustomGraphicsView::itemDropped, this, &MainWindow::handleItemDropped); //подключение слота для обработки события drop и получения координат
+    connect(ui->listView->selectionModel(), &QItemSelectionModel::currentChanged, this, &MainWindow::displayCoordinates); //Подключение слота выбора элемента из QListView и отображения координат
+
+    tableModel = new QStandardItemModel(0, 0, this);
+    ui->tableView->setModel(tableModel);
+
+    addRowsAndColumns(2, 2); //Установка таблицы 2 на 2
+
+}
+
+void MainWindow::updateModel(const QString &itemName) { //Вывод наимонвания иконки в QListView
+    QStringList list = model->stringList();
+    list << itemName;
+    model->setStringList(list);
+}
+
+void MainWindow::addRowsAndColumns(int rows, int cols) { //Добавление фиксированных наименований в таблице
+    tableModel->setRowCount(rows);
+    tableModel->setColumnCount(cols);
+
+    QStandardItem *itemX = new QStandardItem("X");
+    itemX->setEditable(false);
+    tableModel->setItem(0, 0, itemX);
+
+    QStandardItem *itemY = new QStandardItem("Y");
+    itemY->setEditable(false);
+    tableModel->setItem(1, 0, itemY);
+}
+
+void MainWindow::displayCoordinates(const QModelIndex &current, const QModelIndex &previous) { //отображение координат
+    Q_UNUSED(previous)
+
+    if (!tableModel) { //проверка на существование таблицы
+        tableModel = new QStandardItemModel(1, 2, this);
+        ui->tableView->setModel(tableModel);
+        tableModel->setHorizontalHeaderLabels(QStringList() << "X" << "Y");
+    }
+
+       QString iconName = model->data(current, Qt::DisplayRole).toString(); //получение наименование и координат иконки
+
+       QPointF position = iconCoordinates.value(iconName, QPointF(0, 0));
+
+       tableModel->setItem(0, 1, new QStandardItem(QString::number(position.x()))); //установка координат в таблицу
+       tableModel->setItem(1, 1, new QStandardItem(QString::number(position.y())));
+}
+
+void MainWindow::handleItemDropped(const QString &itemName, const QPointF &position) {
+
+    updateModel(itemName);
+    iconCoordinates[itemName] = position; // Сохранение координат в QMap
+
+    qDebug() << "Иконка с именем" << itemName << "была добавлена на позиции" << position; //вывод информации в консоль
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
